922e9c279e8f660d0ef73b6ff3e4f3c14bd4898c
chrisclarkedev <cclarke8904@gmail.com>
2020-12-30 19:38:32 -0500

    functionality of event clicks with event listener methods

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-console.log(btnsOpenModal);
+const closeModal = function () {
+  // Event listener when user clicks (X) button
+  // Modal goes to hidden
+  // Overlay goes to hidden
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  // Event listener when user clicks show modal hidden is removed and modal is shown
+  modal.classList.remove("hidden");
+  // Event listener when user click show modal hidden is removed and modal overlay is display in background
+  overlay.classList.remove("hidden");
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("Button clicked");
-  });
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
